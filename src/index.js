f77e4c7509c58f132a591264b8996e69adad8b85
sagar sharma <sagarkush2003@gmail.com>
2023-08-19 23:39:07 +0530

    First commit:project setup done

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { PORT } = require('./config/serverConfig');
+
+const app = express();
+
+const prepareAndStartServer = () => {
+     app.listen(3001, () => {
+        console.log(`server started  on Port :${PORT}`);
+     })
+}
+prepareAndStartServer();
